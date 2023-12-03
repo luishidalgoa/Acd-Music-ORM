36b33d3eefa8d94388e12f05b3e869478eea7743
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-12-03 16:56:55 +0100

    Update CommentDAO.java

--- src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/DAO/CommentDAO.java
+++ src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/DAO/CommentDAO.java
@@ -1,112 +1,111 @@
 package dev.iesfranciscodelosrios.acdmusic.Model.DAO;
 
-import dev.iesfranciscodelosrios.acdmusic.Connection.ConnectionData;
 import dev.iesfranciscodelosrios.acdmusic.Interfaces.iCommentDAO;
 import dev.iesfranciscodelosrios.acdmusic.Model.Domain.Comment;
+import dev.iesfranciscodelosrios.acdmusic.Connection.ConnectionData;
 
-import java.sql.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommentDAO implements iCommentDAO {
     private static CommentDAO instance;
-    String addCommentQuery="INSERT INTO commentlistusers (id_user, id_reproductionList, date, description) VALUES (?, ?, CURRENT_DATE, ?)";
-    String searchCommentQuery="SELECT id_comment,id_user,id_reproductionList,date,description FROM commentlistusers WHERE id_comment = ?";
-    String deleteCommentQuery="DELETE FROM commentlistusers WHERE id_comment = ?";
-    String searchAllByIdListQuery="SELECT id_comment,id_user,id_reproductionList,date,description FROM commentlistusers WHERE id_reproductionList = ?";
-    private CommentDAO(Connection connection) {
+
+    private CommentDAO() {
     }
 
-
+    public static CommentDAO getInstance() {
+        if (instance == null) {
+            instance = new CommentDAO();
+        }
+        return instance;
+    }
 
     @Override
-    public Comment add(Comment comment){
-        Connection conn= ConnectionData.getConnection();
+    public Comment add(Comment comment) {
+        EntityManager manager = ConnectionData.emf.createEntityManager();
+        EntityTransaction transaction = null;
+
         try {
-            PreparedStatement ps=conn.prepareStatement(addCommentQuery, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1,comment.getUser().getId());
-            ps.setInt(2,comment.getReproductionListId());
-            ps.setString(3,comment.getDescription());
-            ps.executeUpdate();
-            ResultSet rs=ps.getGeneratedKeys();
-            if(rs.next()){
-                comment.setId(rs.getInt(1));
-                return comment;
+            transaction = manager.getTransaction();
+            transaction.begin();
+            manager.persist(comment);
+            transaction.commit();
+            return comment;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }finally {
-            ConnectionData.close();
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
-        return null;
     }
 
     @Override
     public boolean delete(int commentId) {
-        Connection conn= ConnectionData.getConnection();
+        EntityManager manager = ConnectionData.emf.createEntityManager();
+        EntityTransaction transaction = null;
 
-        try (PreparedStatement preparedStatement = conn.prepareStatement(deleteCommentQuery)) {
-            preparedStatement.setInt(1, commentId);
-
-            if(preparedStatement.executeUpdate() > 0){
-                return true;
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            Comment comment = manager.find(Comment.class, commentId);
+            manager.remove(comment);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
             }
-        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
-            ConnectionData.close();
+            return false;
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
-        return false;
     }
 
     @Override
     public List<Comment> searchAllByIdList(int idList) {
-        Connection conn= ConnectionData.getConnection();;
+        EntityManager manager = ConnectionData.emf.createEntityManager();
         List<Comment> comments = new ArrayList<>();
 
-        try (PreparedStatement preparedStatement = conn.prepareStatement(searchAllByIdListQuery)) {
-            preparedStatement.setInt(1, idList);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                comments.add(searchComment(resultSet.getInt("id_comment")));
-            }
-        } catch (SQLException e) {
+        try {
+            TypedQuery<Comment> query = manager.createQuery("SELECT c FROM Comment c WHERE c.reproductionListId = :idList", Comment.class);
+            query.setParameter("idList", idList);
+            comments = query.getResultList();
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            ConnectionData.close();
+        } finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
-        if(comments.isEmpty()){
+
+        if (comments.isEmpty()) {
             return null;
         }
         return comments;
     }
 
-    public Comment searchComment(int idComment){
-        Connection conn= ConnectionData.getConnection();
+    public Comment searchComment(int idComment) {
+        EntityManager manager = ConnectionData.emf.createEntityManager();
         try {
-            PreparedStatement ps=conn.prepareStatement(searchCommentQuery);
-            ps.setInt(1,idComment);
-            ResultSet rs=ps.executeQuery();
-            while (rs.next()){
-                Comment result= new Comment();
-                result.setId(rs.getInt("id_comment"));
-                result.setUser(UserDAO.getInstance().searchById(rs.getInt("id_user")));
-                result.setReproductionListId(rs.getInt("id_reproductionList"));
-                result.setDate(Timestamp.valueOf(rs.getString("date")).toLocalDateTime());
-                result.setDescription(rs.getString("description"));
-                return result;
+            return manager.find(Comment.class, idComment);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (manager != null) {
+                manager.close();
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
-        return null;
-    }
-
-    public static CommentDAO getInstance() {
-        if (instance == null) {
-            instance = new CommentDAO(ConnectionData.getConnection());
-        }
-        return instance;
     }
 }
