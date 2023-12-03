5ed91a5d0dbd8780149d6fa559e26551966d3d23
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-12-03 16:39:24 +0100

    Cambios en artist y metodos searchArtistyIdAlbum hechos

--- src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/Domain/Artist.java
+++ src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/Domain/Artist.java
@@ -1,41 +1,45 @@
 package dev.iesfranciscodelosrios.acdmusic.Model.Domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table
+@Table(name = "artist")
+@PrimaryKeyJoinColumn(name = "ID")
 public class Artist extends User{
     private static final long serialVersionUID = 1L;
-    @Id
-    @Column(name = "ID")
+    @MapsId
+    @Column(name = "ID_ARTIST")
     int id_artist;
     @Column(name = "NACIONALITY")
     String nacionality;
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
+    private List<Album> albums;
 
-    public Artist(int id, String nickName, String name, String lastName, String picture, String email, String password, int id_artist, String nacionality, int id_user) {
+    public Artist(int id, String nickName, String name, String lastName, String picture, String email, String password, int id_artist, String nacionality, int id_user, List<Album> albums) {
         super(id, nickName, name, lastName, picture, email, password);
+        this.albums = albums;
         this.id_artist = super.id;
         this.nacionality = nacionality;
 
     }
 
-    public Artist(String nickName, String name, String lastName, String picture, String email, String password, int id_artist, String nacionality, int id_user) {
+    public Artist(String nickName, String name, String lastName, String picture, String email, String password, int id_artist, String nacionality, int id_user, List<Album> albums) {
         super(nickName, name, lastName, picture, email, password);
         this.id_artist = id_artist;
         this.nacionality = nacionality;
+        this.albums = albums;
     }
 
     public Artist() {
     }
 
-    public Artist(int id, String nickName, String name, String lastName, String picture, String email, int id_artist, String nacionality, int id_user) {
+    public Artist(int id, String nickName, String name, String lastName, String picture, String email, int id_artist, String nacionality, int id_user, List<Album> albums) {
         super(id, nickName, name, lastName, picture, email);
         this.id_artist = id_artist;
         this.nacionality = nacionality;
+        this.albums = albums;
     }
 
     public int getId_artist() {
